3385a32e031596737d14d3ab208c3b1006ba6f01
AyseeD <ayse.delen@yandex.com>
2026-08-12 00:56:57 +0300

    Connect front-end with browse end point in services

---
+++ app/bookstore/webapp/Component.ts
@@ -0,0 +1,25 @@
+import UIComponent from "sap/ui/core/UIComponent";
+import Device from "sap/ui/Device";
+import JSONModel from "sap/ui/model/json/JSONModel";
+
+/**
+ * @namespace booksample
+ */
+export default class Component extends UIComponent{
+    public static metadata = {
+        "interfaces": ["sap.ui.core.IAsyncContentCreation"],
+        "manifest": "json"
+    };
+
+    init(): void{
+        super.init();
+
+        const deviceModel = new JSONModel(Device);
+        deviceModel.setDefaultBindingMode("OneWay");
+        this.setModel(deviceModel, "device");
+    }
+
+    getContentDensityClass(): string {
+        return Device.support.touch ? "sapUiSizeCozy" : "sapUiSizeCompact";
+    }
+}
